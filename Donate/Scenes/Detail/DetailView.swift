30d12ca7192d2c59f1971e673e97bc33a076596e
babarone22 <barbara_barone2@hotmail.com>
2024-02-07 22:25:23 -0300

    alteração de model

---
+++ Donate/Scenes/Detail/DetailView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct DetailView: View {
+    let images = [GridItem(.flexible())]
+    
+    var body: some View {
+        Text("batata")
+    }
+}
+
+#Preview {
+    DetailView()
+}
--- Donate/Service/Model/Animals.swift
+++ Donate/Service/Model/Animals.swift
@@ -2,6 +2,14 @@ import Foundation
 
 struct Animal: Codable, Identifiable {
     var id = UUID()
-    let animalImage: String
-    let animalName: String
+    let image: String
+    let name: String
+    let location: String
+    let age: String
+    let gender: String
+    let specie: String
+    let description: String
+    let breed: String?
+    let phone: String?
+    let email: String?
 }
